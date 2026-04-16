91dee33897c35e476fe8b35ba95f641f3ddc2b70
mario <mario@DESKTOP-T4TCVBA>
2026-04-16 09:54:31 -0400

    Cambio en la clase Empleado

--- MiniMarketProyecto/src/logico/Empleado.java
+++ MiniMarketProyecto/src/logico/Empleado.java
@@ -1,6 +1,7 @@
 package logico;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Empleado extends Persona {
 	private String correo; 
@@ -9,9 +10,12 @@ public class Empleado extends Persona {
 	private LocalDate fechaCreacion; 
 	private boolean estado; 
 	private TipoRol rol;
+	private float sueldo; 
+	private int cantHoras; 
+	private ArrayList<Compra> misVentas; 
 	
 	public Empleado(String nombre, String cedula, String telefono, String direccion, String correo, String password,
-			String usuario, TipoRol rol) {
+			String usuario, TipoRol rol, int cantHoras) {
 		super(nombre, cedula, telefono, direccion);
 		this.correo = correo;
 		this.password = password;
@@ -19,6 +23,9 @@ public class Empleado extends Persona {
 		this.fechaCreacion = LocalDate.now();
 		this.estado = false;
 		this.rol = rol;
+		this.sueldo = 0.0f; 
+		this.cantHoras = cantHoras; 
+		this.misVentas = misVentas; 
 	}
 
 	public String getCorreo() {
@@ -67,6 +74,30 @@ public class Empleado extends Persona {
 
 	public void setRol(TipoRol rol) {
 		this.rol = rol;
+	}
+
+	public float getSueldo() {
+		return sueldo;
+	}
+
+	public void setSueldo(float sueldo) {
+		this.sueldo = sueldo;
+	}
+
+	public int getCantHoras() {
+		return cantHoras;
+	}
+
+	public void setCantHoras(int cantHoras) {
+		this.cantHoras = cantHoras;
+	}
+
+	public ArrayList<Compra> getMisVentas() {
+		return misVentas;
+	}
+
+	public void setMisVentas(ArrayList<Compra> misVentas) {
+		this.misVentas = misVentas;
 	} 
 	
 	
