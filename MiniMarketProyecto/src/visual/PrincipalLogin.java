8f668ca6fdb8416b0512b190ba2265763a3a0985
mario <mario@DESKTOP-T4TCVBA>
2026-04-21 09:06:16 -0400

    Login

---
+++ MiniMarketProyecto/src/visual/PrincipalLogin.java
@@ -0,0 +1,48 @@
+package visual;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class PrincipalLogin extends JFrame {
+
+	private JPanel contentPane;
+	private Dimension dim; 
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PrincipalLogin frame = new PrincipalLogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PrincipalLogin() {
+		setTitle("Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		dim = getToolkit().getScreenSize(); 
+		setSize(dim.width, dim.height - 45);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
